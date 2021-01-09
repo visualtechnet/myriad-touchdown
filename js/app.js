4881dd751862b64ec66d10a83990467f423f182e
Mark Guerrero <mark2579@gmail.com>
2021-01-09 16:29:14 -0500

    feat: refactor click behavior and style

--- js/app.js
+++ js/app.js
@@ -37,18 +37,25 @@
 		displayMemo(memo)
 	}
 
-	function handleMenuList() {
+	/**
+	 * Function to handle Menu Click and to remove and set active section
+	 * @param {object} event Actual Event Target Object
+	 */
+	function handleMenuList(event) {
 		const menuText = this.textContent.toLowerCase()
 		const section = document.querySelector('#' + menuText)
 		const sectionTitle = document.querySelector(`#${menuText} .title`)
+		const sectionList = document.querySelector(`#${menuText} .${menuText}`)
 		const header = document.querySelector('header')
-		const allMenuItems = document.querySelectorAll('.menu-container li')
+		const allMenuItems = document.querySelectorAll('.menu-container li a')
 		const allSectionTitle = document.querySelectorAll('.item-section .title')
+		const allSectionList = document.querySelectorAll('.item-section .section-list')
 
-		const { y } = section.getBoundingClientRect()
+		const yOffset = -140
+		const { top } = section.getBoundingClientRect()
+		const y = top + window.pageYOffset + yOffset;
 
-		//window.scrollTo({ top: y - 200, behavior: 'smooth'})
-		section.scrollIntoView(true, { behavior: 'smooth' })
+		window.scrollTo({ top: y, behavior: 'smooth'})
 
 		header.classList.add('fixed-header')
 
@@ -56,26 +63,39 @@
 			menu.classList.remove('active')
 		})
 
+		allSectionList.forEach(function (section) {
+			section.classList.remove('section-active')
+		})
+
 		allSectionTitle.forEach(function (title) {
 			title.classList.remove('title-active')
 		})
 
 		this.classList.add('active')
+		sectionList.classList.add('section-active')
 		sectionTitle.classList.add('title-active')
+
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
 	}
 
 	function displayMenu() {
-		const liNews = document.querySelector('#liNews')
-		const liPhotos = document.querySelector('#liPhotos')
-		const liMoments = document.querySelector('#liMoments')
-		const liMemo = document.querySelector('#liMemo')
+		const liNews = document.querySelector('#liNews a')
+		const liPhotos = document.querySelector('#liPhotos a')
+		const liMoments = document.querySelector('#liMoments a')
+		const liMemo = document.querySelector('#liMemo a')
 		liNews.addEventListener('click', handleMenuList)
 		liPhotos.addEventListener('click', handleMenuList)
 		liMoments.addEventListener('click', handleMenuList)
 		liMemo.addEventListener('click', handleMenuList)
-
 	}
 
+
+	/**
+	 * Array List of news object to iterate and build
+	 * @param {array} list
+	 */
 	function displayNews(list) {
 		const newsContainer = document.querySelector('.news')
 		const fragmenItem = document.createDocumentFragment()
@@ -101,8 +121,12 @@
 		newsContainer.appendChild(fragmenItem)
 	}
 
+	/**
+	 * Array List of Photos object to iterate and build
+	 * @param {array} list
+	 */
 	function displayPhotos(list) {
-		const photoContainer = document.querySelector('.photos-section')
+		const photoContainer = document.querySelector('.photos')
 		const fragmenItem = document.createDocumentFragment()
 
 		list.forEach(function (item) {
@@ -117,6 +141,10 @@
 		photoContainer.appendChild(fragmenItem)
 	}
 
+	/**
+	 * Array List of Moments object to iterate and build
+	 * @param {array} list
+	 */
 	function displayMoments(list) {
 		const momentContainer = document.querySelector('.moments')
 		const fragmenItem = document.createDocumentFragment()
@@ -133,6 +161,10 @@
 		momentContainer.appendChild(fragmenItem)
 	}
 
+	/**
+	 * Array List of Memorabilia object to iterate and build
+	 * @param {array} list
+	 */
 	function displayMemo(list) {
 		const memoContainer = document.querySelector('.memorabilia')
 		const fragmenItem = document.createDocumentFragment()
@@ -161,89 +193,84 @@
 		})
 	})
 
-	// window.addEventListener('scroll', function () {
-	// 	const header = document.querySelector('header')
-	// 	const menuItems = document.querySelectorAll('.menu-container li')
-	// 	const scroll2Top = document.querySelector('.scroll2Top')
-	// 	const titleNews = document.querySelector('.title-news')
-	// 	const titlePhotos = document.querySelector('.title-photos')
-	// 	const titlePoll = document.querySelector('.title-poll')
-	// 	const titleMoments = document.querySelector('.title-moments')
-	// 	const titleVideos = document.querySelector('.title-videos')
-	// 	const titlesMarkup = document.querySelectorAll('.title')
+	/**
+	 * Listener to highlight section when scrolled to. Highlight includes both the title and the unordered list
+	 */
+	window.addEventListener('scroll', function() {
+		const yOffset = -160
+		const yOffsetUBound = 160
+		// const { top } = section.getBoundingClientRect()
+		// const y = top + window.pageYOffset + yOffset;
+		const header = document.querySelector('header')
 
-	// 	if (header.classList.contains('header-hidden')) {
-	// 		header.classList.remove('header-hidden')
-	// 	}
+		const menuItems = document.querySelectorAll('.menu-container li a')
+		const titleNews = document.querySelector('.title-news')
+		const titlePhotos = document.querySelector('.title-photos')
+		const titleMoments = document.querySelector('.title-moments')
+		const titleMemo = document.querySelector('.title-memorabilia')
 
-	// 	header.classList.add('fixed-header')
+		const allSectionTitle = document.querySelectorAll('.item-section .title')
+		const allSectionList = document.querySelectorAll('.item-section .section-list')
 
-	// 	menuItems.forEach((activeItem) => {
-	// 		activeItem.classList.remove('active')
-	// 	})
+		header.classList.add('fixed-header')
+		menuItems.forEach(function (menu) {
+			menu.classList.remove('active')
+		})
+		allSectionList.forEach(function (section) {
+			section.classList.remove('section-active')
+		})
 
-	// 	if (window.scrollY == 0) {
-	// 		header.classList.remove('fixed-header')
-	// 		scroll2Top.classList.remove('visible')
-	// 	}
+		allSectionTitle.forEach(function (title) {
+			title.classList.remove('title-active')
+		})
 
-	// 	if (menuItems.length === 0) {
-	// 		return
-	// 	}
 
-	// 	if (
-	// 		window.scrollY >= 320 &&
-	// 		scroll2Top.classList.contains('visible') == false
-	// 	) {
-	// 		scroll2Top.classList.add('visible')
-	// 	}
+		const scrollNews = titleNews.getBoundingClientRect().top + window.pageYOffset + yOffset
+		if (window.scrollY > scrollNews && window.scrollY < (scrollNews + yOffsetUBound )) {
+			const menuText = menuItems[0].innerHTML.toLowerCase()
+			const sectionTitle = document.querySelector(`#${menuText} .title`)
+			const sectionList = document.querySelector(`#${menuText} .${menuText}`)
+			menuItems[0].classList.add('active')
+			sectionList.classList.add('section-active')
+			sectionTitle.classList.add('title-active')
+		}
 
-	// 	titlesMarkup.forEach((title) => {
-	// 		if (title.classList.contains('title-active')) {
-	// 			title.classList.remove('title-active')
-	// 		}
-	// 	})
+		const scrollPhotos = titlePhotos.getBoundingClientRect().top + window.pageYOffset + yOffset
+		//console.log('Photos ', window.scrollY, scrollPhotos, scrollPhotos + yOffsetUBound)
+		if (window.scrollY > scrollPhotos && window.scrollY < (scrollPhotos + yOffsetUBound )) {
+			const menuText = menuItems[1].innerHTML.toLowerCase()
+			const sectionTitle = document.querySelector(`#${menuText} .title`)
+			const sectionList = document.querySelector(`#${menuText} .${menuText}`)
 
-	// 	if (
-	// 		window.scrollY >= 0 &&
-	// 		window.scrollY < 1290 &&
-	// 		menuItems[0].classList.contains('active') == false
-	// 	) {
-	// 		menuItems[0].classList.add('active')
-	// 		titleNews.classList.add('title-active')
-	// 	} else if (
-	// 		window.scrollY >= 1290 &&
-	// 		window.scrollY < 1835 &&
-	// 		menuItems[1].classList.contains('active') == false
-	// 	) {
-	// 		menuItems[1].classList.add('active')
-	// 		titlePhotos.classList.add('title-active')
-	// 	} else if (
-	// 		window.scrollY >= 1835 &&
-	// 		window.scrollY < 2355 &&
-	// 		menuItems[2].classList.contains('active') == false
-	// 	) {
-	// 		menuItems[2].classList.add('active')
-	// 		titlePoll.classList.add('title-active')
-	// 	} else if (
-	// 		window.scrollY >= 2355 &&
-	// 		window.scrollY < 2618 &&
-	// 		menuItems[3].classList.contains('active') == false
-	// 	) {
-	// 		menuItems[3].classList.add('active')
-	// 		titleMoments.classList.add('title-active')
-	// 	} else if (
-	// 		window.scrollY >= 2618 &&
-	// 		menuItems[4].classList.contains('active') == false
-	// 	) {
-	// 		menuItems[4].classList.add('active')
-	// 		titleVideos.classList.add('title-active')
-	// 	}
+			menuItems[1].classList.add('active')
+			sectionList.classList.add('section-active')
+			sectionTitle.classList.add('title-active')
+		}
 
-	// 	setTimeout(function () {
-	// 		const header = document.querySelector('header')
+		const scrollMoments = titleMoments.getBoundingClientRect().top + window.pageYOffset + yOffset
+		//console.log('Moments ', window.scrollY, scrollMoments, scrollMoments + yOffsetUBound)
+		if (window.scrollY > scrollMoments && window.scrollY < (scrollMoments + yOffsetUBound )) {
+			const menuText = menuItems[2].innerHTML.toLowerCase()
+			const sectionTitle = document.querySelector(`#${menuText} .title`)
+			const sectionList = document.querySelector(`#${menuText} .${menuText}`)
 
-	// 		if (window.scrollY > 300) header.classList.add('header-hidden')
-	// 	}, 2000)
-	// })
+			menuItems[2].classList.add('active')
+			sectionList.classList.add('section-active')
+			sectionTitle.classList.add('title-active')
+		}
+
+		const scrollMemo = titleMemo.getBoundingClientRect().top + window.pageYOffset + yOffset
+		console.log('memo ', window.scrollY, scrollMoments, scrollMoments + yOffsetUBound)
+		if (window.scrollY > scrollMemo && window.scrollY < (scrollMemo + yOffsetUBound )) {
+			const menuText = menuItems[3].innerHTML.toLowerCase()
+			const sectionTitle = document.querySelector(`#${menuText} .title`)
+			const sectionList = document.querySelector(`#${menuText} .${menuText}`)
+
+			menuItems[3].classList.add('active')
+			sectionList.classList.add('section-active')
+			sectionTitle.classList.add('title-active')
+		}
+
+	})
+
 })()
